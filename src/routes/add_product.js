bb5aeb49f54b29e1a16fec8ce2e5158f9bf0c225
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2021-11-16 21:40:31 +0700

    sdf

--- src/routes/add_product.js
+++ src/routes/add_product.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const route=express.Router()
 
-const AddProductController=require('../conttrollers/AddProductController.js')
+const AddProductController=require('../conttrollers/AddProductController')
 
 route.get('/',AddProductController.index)
 module.exports=route
