4d13f61b4093a9b7f43d178b74a917fcee8ed92f
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2021-11-21 14:13:53 +0700

    sdfsdfsd

--- src/conttrollers/ProductController.js
+++ src/conttrollers/ProductController.js
@@ -1,13 +1,13 @@
 const Product =require('../database/models/Product')
 class ProductController{
-    async addProduct(req,res){
+    async addProduct(req,res){   // get
         res.render('add_product')
     }
-    async createProduct(req,res){
-        console.log(req.body.name)
+    async createProduct(req,res){ // post
+        console.log(req.body)
         const file = req.file
         const path='/uploads/'+file.filename
-        const newProduct=new Product({})
+        const newProduct=new Product({...req.body})
         if (!file) {
             const error = new Error('Please upload a file')
             error.httpStatusCode = 400
@@ -18,8 +18,14 @@ class ProductController{
     async editProduct(req,res){
         res.render('edit_product')
     }
-    async viewProduct(req,res){
-        res.render('product_list')
+    async viewProduct(req,res){ // get
+        try{
+            const allProduct=await Product.find({})
+            res.render('product_list',{allProduct})
+        }
+        catch(e){
+            res.render('404')
+        }
     }
 }
 module.exports=new ProductController
