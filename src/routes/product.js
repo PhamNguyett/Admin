02a00d6c16c799318bf75d9f9be39ec46c3db52b
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2021-11-21 13:20:30 +0700

    sdfsd

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,10 +1,23 @@
+const publicURL=require('../public/url')
+const multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    cb(null, publicURL+'/uploads')
+    },
+    filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+    }
+})
+var upload = multer({ storage: storage })
+
+
 const express=require('express')
 const route=express.Router()
 
 const ProductController=require('../conttrollers/ProductController')
 
 route.get('/all',ProductController.viewProduct)
-route.post('/add',ProductController.creaProduct)
+route.post('/add',upload.single('image'),ProductController.createProduct)
 route.get('/edit',ProductController.editProduct)
 route.get('/add',ProductController.addProduct)
 module.exports=route
