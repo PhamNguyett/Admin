6fc6d5f911b87986fe080701c164a235c2bfea4a
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2021-11-21 13:20:30 +0700

    sdfsd

--- src/conttrollers/ProductController.js
+++ src/conttrollers/ProductController.js
@@ -1,14 +1,19 @@
-const formidable = require('formidable');
+const Product =require('../database/models/Product')
 class ProductController{
     async addProduct(req,res){
-        const form = formidable();
-        
         res.render('add_product')
     }
-    async creaProduct(req,res){
-        console.log(req.body)
-        
-        res.render('add_product')
+    async createProduct(req,res){
+        console.log(req.body.name)
+        const file = req.file
+        const path='/uploads/'+file.filename
+        const newProduct=new Product({})
+        if (!file) {
+            const error = new Error('Please upload a file')
+            error.httpStatusCode = 400
+            return next(error)
+        }
+        res.render('test',{src:path})
     }
     async editProduct(req,res){
         res.render('edit_product')
@@ -16,6 +21,5 @@ class ProductController{
     async viewProduct(req,res){
         res.render('product_list')
     }
-    
 }
 module.exports=new ProductController
