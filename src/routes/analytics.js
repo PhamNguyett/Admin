d916115fecc55b9005ab9e7a67117aae43298f7d
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2021-12-02 08:04:39 +0700

    ff

--- src/routes/analytics.js
+++ src/routes/analytics.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const route=express.Router()
 
-const AnalyticsController=require('../conttrollers/AnalyticsController.js')
+const AnalyticsController=require('../controllers/AnalyticsController.js')
 
 route.get('/',AnalyticsController.index)
 module.exports=route
