8f36457ec5951d9330ccf1dfea2f5be63516bb37
PhamNguyett <ptnguyetttt@gmail.com>
2021-12-28 23:40:51 +0700

    morder

---
+++ src/controllers/OrderController.js
@@ -0,0 +1,17 @@
+const Order=require('../database/models/Order')
+const {MultipleMongooseToObject,MongooseToObject}=require('../ultil/mongoose')
+class OrderController{
+    async index(req,res){
+        try{
+            const order = await Order.find({})
+            res.render('order',{
+                order:MultipleMongooseToObject(order)),
+            })
+        }
+        catch(e){
+            console.log(e)
+            res.render('404')
+        }
+    }
+}
+module.exports=new OrderController
