7803940b018c1f8994921a08ad4e1394de0406cd
Your Name <you@example.com>
2022-01-03 23:09:13 +0700

    order

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -1,9 +1,11 @@
+const { accessSync } = require('fs')
 const Order=require('../database/models/Order')
 const {MultipleMongooseToObject,MongooseToObject}=require('../ultil/mongoose')
 class OrderController{
     async index(req,res){
         try{
-            const allOrder = await Order.find({})
+            const allOrder = await Order.find({}).populate('user')
+            console.log(allOrder)
             res.render('order',{
                 allOrder:MultipleMongooseToObject(allOrder),
             })
@@ -11,6 +13,54 @@ class OrderController{
         catch(e){
             console.log(e)
             res.render('404')
+        }
+    }
+
+    async waiting(req,res){
+        try{
+            const waitingOrder = await Order.find({status:false})
+            res.render('order',{
+                allOrder:MultipleMongooseToObject(waitingOrder),
+            })
+        }
+        catch(e){
+            console.log(e)
+            res.render('404')
+        }
+    }
+    
+    async processed(req,res){
+        try{
+            const proOrder = await Order.find({status:true})
+            res.render('order',{
+                allOrder:MultipleMongooseToObject(proOrder)
+            })
+        }
+        catch(e){
+            console.log(e)
+            res.render('404')
+        }
+
+    }
+
+    async accept(req,res){
+        try{
+            const accOrder = await Order.findOne({_id:req.params.id,status:false})
+            console.log(req.params)
+            console.log(accOrder)
+            if(accOrder)
+            {
+                accOrder.status = true
+                await accOrder.save()
+                res.status(200).json({success:true})
+            }
+            else{
+                res.status(404).json({success:false,message:"Invalid order"})
+            }
+
+        }
+        catch(e){
+            res.status(404).json({success:false})
         }
     }
 }
--- src/routes/login.js
+++ src/routes/login.js
@@ -5,7 +5,7 @@ const LoginController=require('../controllers/LoginController')
 const passport = require('../controllers/helper/auth')
 
 route.get('/',LoginController.login)
-route.post('/',passport.authenticate('local',{ successRedirect: '/product',failureRedirect: '/login' }))
+route.post('/',passport.authenticate('local',{ successRedirect: '/order',failureRedirect: '/login' }))
 
 route.get('/resetpassword',LoginController.reset)
 module.exports=route
