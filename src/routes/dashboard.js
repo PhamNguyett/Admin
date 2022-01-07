fef19ff3f461ea724813e359dc742f8e36dae7d1
PhamNguyett <ptnguyetttt@gmail.com>
2022-01-07 17:09:16 +0700

    fix-menu

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const route=express.Router()
 
-const DashboardController=require('../controllers/DashboardController')
+const {DashboardController}=require('../controllers')
 
 route.get('/',DashboardController.index)
 module.exports=route
