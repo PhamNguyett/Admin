e9cebd20314c2bc0b52702e924cdb01f3edd1a59
PhamNguyett <ptnguyetttt@gmail.com>
2022-01-08 16:45:40 +0700

    add-category

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,7 +1,9 @@
 const express=require('express')
 const route=express.Router()
+const multer=require('../middlewares/multer')
 
 const {CategoryController}=require('../controllers')
 
 route.get('/',CategoryController.index)
+route.post('/add',multer.single('image'),CategoryController.add)
 module.exports=route
